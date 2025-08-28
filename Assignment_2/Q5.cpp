06e3b2b5dad0084839ebfb236f85abc419d81954
Dhruv Srivastava <69293452+ChaoticNebula5@users.noreply.github.com>
2025-08-28 10:14:40 +0530

    Create Q5.cpp

---
+++ Assignment_2/Q5.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+void set(int A[], int n, int i, int j, int x) {
+    if (i == j) 
+        A[i-1] = x; 
+}
+
+int get(int A[], int n, int i, int j) {
+    if (i == j) return A[i-1];
+    else return 0;
+}
+
+void display(int A[], int n) {
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (i == j) cout << A[i-1] << " ";
+            else cout << "0 ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int n = 4;
+    int A[4] = {0};  
+
+    set(A, n, 1, 1, 5);
+    set(A, n, 2, 2, 8);
+    set(A, n, 3, 3, 9);
+    set(A, n, 4, 4, 12);
+
+    cout << "Diagonal Matrix:" << endl;
+    display(A, n);
+
+    return 0;
+}
